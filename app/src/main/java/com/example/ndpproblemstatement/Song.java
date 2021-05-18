e4b4b05f190f8e3d6bafcde49ffe36ed220f6574
19042228 <19042228@myrp.edu.sg>
2021-05-18 13:14:21 +0800

    Song Class

--- app/src/main/java/com/example/ndpproblemstatement/Song.java
+++ app/src/main/java/com/example/ndpproblemstatement/Song.java
@@ -1,4 +1,36 @@
 package com.example.ndpproblemstatement;
 
-public class Song {
+import java.io.Serializable;
+
+public class Song implements Serializable {
+    private int _id;
+    private String title, singers;
+    private int year, stars;
+
+    public Song (String title,String singers, int year, int stars){
+        this.title = title;
+        this.singers = singers;
+        this.year = year;
+        this.stars = stars;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getStars() {
+        return stars;
+    }
 }
